f02ce74f4652da16503f5443e394fad1ba602d2a
LinusAltemark <Linalt0905@gmail.com>
2024-10-07 11:03:29 +0200

    ProductRecord Kod fixat

--- src/main/java/org/example/warehouse/ProductRecord.java
+++ src/main/java/org/example/warehouse/ProductRecord.java
@@ -1,5 +1,22 @@
 package org.example.warehouse;
 
-public class ProductRecord {
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ProductRecord(UUID uuid, String name, Category category, BigDecimal price){
+
+    public ProductRecord(UUID uuid, String name, Category category, BigDecimal price) {
+        if (name == null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("Product name cannot be null or empty");
+        }
+        if (category == null) {
+            throw new IllegalArgumentException("Category cannot be null");
+        }
+
+        this.uuid = uuid != null ? uuid : UUID.randomUUID();
+        this.name = name;
+        this.category = category;
+        this.price = price != null ? price : BigDecimal.ZERO;
+    }
 
 }
